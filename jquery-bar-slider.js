e4ba3f7a0c99b42265a9dde474b737ba58716e61
Cliffmeister <jaycliff.a@agentsofvalue.com>
2016-06-09 15:22:01 +0800

    Copyright update

--- jquery-bar-slider.js
+++ jquery-bar-slider.js
@@ -1,5 +1,5 @@
 /*
-    Copyright 2015 Jaycliff Arcilla of Eversun Software Philippines Corporation
+    Copyright 2016 Jaycliff Arcilla of Eversun Software Philippines Corporation
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
