dd45e42d0371252db75831390b9980d995904b51
Cliffmeister <jaycliff.a@agentsofvalue.com>
2016-08-18 06:44:02 +0800

    Update

--- jquery-bar-slider.js
+++ jquery-bar-slider.js
@@ -433,9 +433,11 @@ if (typeof String.prototype.trim !== "function") {
                     .off('mouseup touchend', docWinEventHandler);
             };
             (function () {
-                var is_default_prevented = false;
+                var is_default_prevented = false, is_propagation_stopped = false, is_immediate_propagation_stopped = false;
                 function helper(event) {
                     is_default_prevented = event.isDefaultPrevented();
+                    is_propagation_stopped = event.isPropagationStopped();
+                    is_immediate_propagation_stopped = event.isImmediatePropagationStopped();
                 }
                 genericEventHandler = function (event) {
                     var nowX, nowY, base, dimension, rate;
@@ -509,6 +511,11 @@ if (typeof String.prototype.trim !== "function") {
                         $bar_slider_object.one(event_type, helper).triggerHandler(event_type); // See if $bar_slider_object event has been default-prevented
                         bs_wrap_do_not_trigger_map[event_type] = false;
                     }
+                    if (is_immediate_propagation_stopped) {
+                        event.isImmediatePropagationStopped();
+                    } else if (is_propagation_stopped) {
+                        event.stopPropagation();
+                    }
                     if (is_default_prevented) {
                         return;
                     }
@@ -567,6 +574,7 @@ if (typeof String.prototype.trim !== "function") {
                                     rate = 0;
                                 } else {
                                     event.preventDefault();
+                                    event.stopPropagation();
                                 }
                             } else {
                                 rate = (((properties.value + properties.step) - min_sub) / (properties.max - min_sub));
@@ -574,6 +582,7 @@ if (typeof String.prototype.trim !== "function") {
                                     rate = 1;
                                 } else {
                                     event.preventDefault();
+                                    event.stopPropagation();
                                 }
                             }
                             moveSlider(rate);
@@ -589,6 +598,7 @@ if (typeof String.prototype.trim !== "function") {
                                     rate = 0;
                                 } else {
                                     event.preventDefault();
+                                    event.stopPropagation();
                                 }
                             } else {
                                 rate = (((properties.value + properties.step) - min_sub) / (properties.max - min_sub));
@@ -596,6 +606,7 @@ if (typeof String.prototype.trim !== "function") {
                                     rate = 1;
                                 } else {
                                     event.preventDefault();
+                                    event.stopPropagation();
                                 }
                             }
                             moveSlider(rate);
